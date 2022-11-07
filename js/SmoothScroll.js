621c4c19633878ddc38fed51dcd7a6989cad75f8
Allen Chen <allen.william.chen@gmail.com>
2022-11-07 11:02:08 -0600

    update smooth scroll, resume wording

--- js/SmoothScroll.js
+++ js/SmoothScroll.js
@@ -12,8 +12,8 @@ var defaultOptions = {
 
     // Scrolling Core
     frameRate        : 150, // [Hz]
-    animationTime    : 400, // [px]
-    stepSize         : 120, // [px]
+    animationTime    : 100, // [px]
+    stepSize         : 10, // [px]
 
     // Pulse (less tweakable)
     // ratio of "tail" to "acceleration"
